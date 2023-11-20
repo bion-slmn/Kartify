16bcfc5b37d9884aae5adc32db9bb45d273ba664
bion-slmn <bionsol25@gmail.com>
2023-11-20 22:33:08 +0300

    organized into folders

--- vendors/Kenya_computer_vendor.py
+++ vendors/Kenya_computer_vendor.py
@@ -1,34 +1,33 @@
 #!/usr/bin/python3
-'''this module define a vendor call Glantix'''
+''' this defines a class KenyaComputer'''
 from vendor import Vendor
 from bs4 import BeautifulSoup
 
 
-class Glantix(Vendor):
-    ''' this class defines a vendor called kilimall'''
+class Kenyacomputer(Vendor):
+    ''' this class defines acomputer sold at Kenya computer'''
 
     def load_items(self, item):
-        '''it loads all the item of the vendor from the
-        file and stores in memory
+        '''it loads all the item of the vendor from the file
+        and stores in memory
 
         Parameter
         - item (str, optional): Type of items to load.
-        Can be 'laptop' or 'desktop'.
+        Can be 'laptop' or 'desktop'
         '''
-        item_file = 'static_data/glantix_laptop' if item == 'laptop' \
-                    else 'static_data/glantix_desktop'
+        item_file = '../static_data/Kenya_computers_laptop' if item == 'laptop' \
+                    else '../static_data/Kenya_computers_desktop'
 
         with open(item_file, 'r') as f:
             soup = BeautifulSoup(f, 'html.parser')
-            all_div = soup.find_all('div', {'class': 'product-card'})
+            all_div = soup.find_all('div', {'class': 'col-inner'})
             for laptop in all_div:
                 new = {}
-                name = laptop.a.get('title')
+                name = laptop.img.get('alt')
                 new['name'] = name
                 new['link'] = laptop.a.get('href')
                 new['img_link'] = laptop.img.get('src')
-                new['price'] = laptop.find('span', {'special-price'}).text.strip('\n KSH\xa0')
-                new['Vendor'] = self.__class__.__name__
+                new['price'] = laptop.find('bdi').text.strip('KSHh\n     ')
 
                 key = '{}.{}'.format(name, self.__class__.__name__)
                 if item == 'laptop':
--- vendors/glantix_vendor.py
+++ vendors/glantix_vendor.py
@@ -1,33 +1,34 @@
 #!/usr/bin/python3
-''' this defines a class KenyaComputer'''
+'''this module define a vendor call Glantix'''
 from vendor import Vendor
 from bs4 import BeautifulSoup
 
 
-class Kenyacomputer(Vendor):
-    ''' this class defines acomputer sold at Kenya computer'''
+class Glantix(Vendor):
+    ''' this class defines a vendor called kilimall'''
 
     def load_items(self, item):
-        '''it loads all the item of the vendor from the file
-        and stores in memory
+        '''it loads all the item of the vendor from the
+        file and stores in memory
 
         Parameter
         - item (str, optional): Type of items to load.
-        Can be 'laptop' or 'desktop'
+        Can be 'laptop' or 'desktop'.
         '''
-        item_file = 'static_data/Kenya_computers_laptop' if item == 'laptop' \
-                    else 'static_data/Kenya_computers_desktop'
+        item_file = '../static_data/glantix_laptop' if item == 'laptop' \
+                    else '../static_data/glantix_desktop'
 
         with open(item_file, 'r') as f:
             soup = BeautifulSoup(f, 'html.parser')
-            all_div = soup.find_all('div', {'class': 'col-inner'})
+            all_div = soup.find_all('div', {'class': 'product-card'})
             for laptop in all_div:
                 new = {}
-                name = laptop.img.get('alt')
+                name = laptop.a.get('title')
                 new['name'] = name
                 new['link'] = laptop.a.get('href')
                 new['img_link'] = laptop.img.get('src')
-                new['price'] = laptop.find('bdi').text.strip('KSHh\n     ')
+                new['price'] = laptop.find('span', {'special-price'}).text.strip('\n KSH\xa0')
+                new['Vendor'] = self.__class__.__name__
 
                 key = '{}.{}'.format(name, self.__class__.__name__)
                 if item == 'laptop':

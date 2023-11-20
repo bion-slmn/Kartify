61b57506bc837d8e9c257b466b1eea82fc386c86
bion-slmn <bionsol25@gmail.com>
2023-11-20 18:25:10 +0300

    pycodestyle

--- glantix_vendor.py
+++ glantix_vendor.py
@@ -3,20 +3,24 @@
 from vendor import Vendor
 from bs4 import BeautifulSoup
 
+
 class Glantix(Vendor):
     ''' this class defines a vendor called kilimall'''
 
     def load_items(self, item):
-        '''it loads all the item of the vendor from the file and stores in memory
+        '''it loads all the item of the vendor from the
+        file and stores in memory
 
         Parameter
-        - item (str, optional): Type of items to load. Can be 'laptop' or 'desktop'.
+        - item (str, optional): Type of items to load.
+        Can be 'laptop' or 'desktop'.
         '''
-        item_file = 'glantix_laptop' if item == 'laptop' else 'glantix_desktop'
+        item_file = 'static_data/glantix_laptop' if item == 'laptop' \
+                    else 'static_data/glantix_desktop'
 
         with open(item_file, 'r') as f:
             soup = BeautifulSoup(f, 'html.parser')
-            all_div = soup.find_all('div', {'class':'product-card'})
+            all_div = soup.find_all('div', {'class': 'product-card'})
             for laptop in all_div:
                 new = {}
                 name = laptop.a.get('title')
@@ -25,7 +29,7 @@ class Glantix(Vendor):
                 new['img_link'] = laptop.img.get('src')
                 new['price'] = laptop.find('span', {'special-price'}).text.strip('\n KSH\xa0')
                 new['Vendor'] = self.__class__.__name__
-                
+
                 key = '{}.{}'.format(name, self.__class__.__name__)
                 if item == 'laptop':
                     self._laptops[key.lower()] = new

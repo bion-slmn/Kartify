ffcbafe38d5e6735da5773b801b7a81380bb5ee0
Lucas Sangura <sanguraowens@gmail.com>
2023-12-06 22:35:21 +0300

    Fix styles

--- flask_app/static/scripts/search.js
+++ flask_app/static/scripts/search.js
@@ -1,44 +1,44 @@
 $(function () {
-    
+
     const searchInput = $("#searchInput");
 
     function getItems () {
         let name = searchInput.val();
-	let vendor;
-	let link;
-	        
-	let value = $('#filterBy').val();
-	let searchLink = "http://localhost:5000/api/v1/search/";
-	let catergoryLink = "http://localhost:5000/api/v1/";
-	name = name.toLowerCase();
+        let vendor;
+        let link;
 
-	 if (name.includes('laptop') || name.includes('desktop')) {
-		 if (name.includes('laptop')) {
-			 name = 'laptop';
-		 } else {
-		         name = 'desktop';
-		 }
-		 if (value === 'VendorGroup') {
-			 link = catergoryLink + name;
-		 } else {
-			 link = catergoryLink + name + '/' + value;
-			}
-	 }
-	        
-	  else if (value != 'VendorGroup') {
-		  vendor = value;
-		  link = searchLink + name + '/' + vendor;
-	  }
-	    else {
-		    link = searchLink + name;
-		}
-	//	    alert(link)
+        let value = $('#filterBy').val();
+        let searchLink = "http://localhost:5000/api/v1/search/";
+        let catergoryLink = "http://localhost:5000/api/v1/";
+        name = name.toLowerCase();
+
+        if (name.includes('laptop') || name.includes('desktop')) {
+            if (name.includes('laptop')) {
+                name = 'laptop';
+            } else {
+                name = 'desktop';
+            }
+            if (value === 'VendorGroup') {
+                link = catergoryLink + name;
+            } else {
+                link = catergoryLink + name + '/' + value;
+            }
+        }
+
+        else if (value != 'VendorGroup') {
+            vendor = value;
+            link = searchLink + name + '/' + vendor;
+        }
+        else {
+            link = searchLink + name;
+        }
+        //	    alert(link)
         $.ajax({
             type: "GET",
             url: link,
             success: function (data) {
                 const itemsDiv = $("#searchResults");
-		itemsDiv.empty(); // clear the previous search items
+                itemsDiv.empty(); // clear the previous search items
                 const keys = Object.keys(data)
                 keys.forEach((key, index) => {
                     const item = data[key];
@@ -54,7 +54,7 @@ $(function () {
                         "            <img src=" + imgUrl + " alt='Image of " + itemName + "'>",
                         "        </div>",
                         "        <div class='ItemInfo'>",
-                            "        <span class='ItemName'>" + itemName + "</span>",
+                        "        <span class='ItemName'>" + itemName + "</span>",
                         "            <span class='ItemVendor'>From " + itemVendor + "</span>",
                         "            <span class='ItemPrice'> KSH " + itemPrice + "</span>",
 
@@ -68,7 +68,7 @@ $(function () {
                     itemsDiv.append(article);
                 })
             },
-            fail: function (error) {console.log(error)}
+            fail: function (error) { console.log(error) }
         })
     }
     $("#searchButton").click(() => getItems());

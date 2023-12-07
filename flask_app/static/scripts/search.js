604047be04782d55872528485524bc23a0bb36ef
Lucas Sangura <sanguraowens@gmail.com>
2023-12-07 19:19:18 +0300

    Implement filter and order by

--- flask_app/static/scripts/search.js
+++ flask_app/static/scripts/search.js
@@ -1,43 +1,16 @@
 $(function () {
 
     const searchInput = $("#searchInput");
+    const itemsDiv = $("#searchResults");
 
     function getItems () {
-        let name = searchInput.val();
-        let vendor;
-        let link;
-
-        let value = $('#filterBy').val();
+        let name = searchInput.val().toLowerCase();
         let searchLink = "http://localhost:5000/api/v1/search/";
-        let catergoryLink = "http://localhost:5000/api/v1/";
-        name = name.toLowerCase();
 
-        if (name.includes('laptop') || name.includes('desktop')) {
-            if (name.includes('laptop')) {
-                name = 'laptop';
-            } else {
-                name = 'desktop';
-            }
-            if (value === 'VendorGroup') {
-                link = catergoryLink + name;
-            } else {
-                link = catergoryLink + name + '/' + value;
-            }
-        }
-
-        else if (value != 'VendorGroup') {
-            vendor = value;
-            link = searchLink + name + '/' + vendor;
-        }
-        else {
-            link = searchLink + name;
-        }
-        //	    alert(link)
         $.ajax({
             type: "GET",
-            url: link,
+            url: searchLink + name,
             success: function (data) {
-                const itemsDiv = $("#searchResults");
                 itemsDiv.empty(); // clear the previous search items
                 const keys = Object.keys(data)
                 keys.forEach((key, index) => {
@@ -48,16 +21,15 @@ $(function () {
                     const itemName = item["name"];
                     const itemPrice = item["price"];
                     const article = $([
-                        "<article class='ItemCard' id=''>",
+                        `<article class='ItemCard' vendor=${itemVendor.toLowerCase()} id=''>`,
                         "    <a class='ItemLink' href='" + itemLink + "'>",
                         "        <div class='ItemImage'>",
                         "            <img src=" + imgUrl + " alt='Image of " + itemName + "'>",
                         "        </div>",
                         "        <div class='ItemInfo'>",
-                        "        <span class='ItemName'>" + itemName + "</span>",
-                        "            <span class='ItemVendor'>From " + itemVendor + "</span>",
-                        "            <span class='ItemPrice'> KSH " + itemPrice + "</span>",
-
+                        "            <div class='ItemName'>" + itemName + "</div>",
+                        "            <div>From&nbsp<span class='ItemVendor'>" + itemVendor + "</span></div>",
+                        "            <div>KSH&nbsp<span class='ItemPrice'>" + itemPrice + "</span></div>",
                         "        </div>",
                         "    </a>",
                         "</article>"
@@ -67,10 +39,51 @@ $(function () {
                     }
                     itemsDiv.append(article);
                 })
+                orderItems();
+                filterItems();
             },
             fail: function (error) { console.log(error) }
         })
     }
+    function orderItems () {
+        const orderVal = $("#orderBy").val();
+        const sortedItems = $(".ItemCard").sort(function (a, b) {
+            const v1 = $(a).find(`.${orderVal}`).text();
+            const v2 = $(b).find(`.${orderVal}`).text();
+            if (orderVal == "ItemPrice") {
+                const p1 = v1.replace(",", "");
+                const p2 = v2.replace(",", "");
+                return parseInt(p1) - parseInt(p2);
+            }
+            if (v1 < v2) {
+                return -1;
+            }
+            if (v1 == v2) {
+                return 0;
+            }
+            return 1;
+        });
+        itemsDiv.empty();
+        itemsDiv.append(sortedItems);
+        $("#displayOrder").val("0")
+    }
+    function displayOrder () {
+        const items = $(itemsDiv).find(".ItemCard");
+        itemsDiv.empty();
+        itemsDiv.append(items.toArray().reverse());
+    }
+    function filterItems () {
+        const filterVal = $("#filterBy").val().toLowerCase();
+        if (filterVal == "none") {
+            $(itemsDiv).find('.ItemCard').css("display", "flex")
+            return;
+        }
+        $(itemsDiv).find('.ItemCard').css("display", "none")
+        $(itemsDiv).find(`[vendor="${filterVal}"]`).css("display", "flex")
+    }
     $("#searchButton").click(() => getItems());
+    $("#orderBy").change(() => orderItems());
+    $("#displayOrder").change(() => displayOrder());
+    $("#filterBy").change(() => filterItems());
     getItems();
 })
